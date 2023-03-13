b574fccb296cf790504c8eca5002e41ce47b9fe6
Сергей Бернякович <sergejbernakovic@MacBook-Air-Sergej.local>
2023-03-13 20:52:34 +0500

    hooks task 4 useViwportSize

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,6 +1,7 @@
 import Task1 from './components/Task1'
 import Task2 from './components/Task2'
 import Task3 from './components/Task3'
+import Task4 from './components/Task4'
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Task1 />
 			<Task2 />
 			<Task3 />
+			<Task4 />
 		</div>
 	)
 }
