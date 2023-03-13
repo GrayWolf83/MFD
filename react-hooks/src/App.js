bca0f60dd88619c2d8405e9972155d278786ea6b
Сергей Бернякович <sergejbernakovic@MacBook-Air-Sergej.local>
2023-03-13 15:23:25 +0500

    hooks task 1 useFetch

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,5 +1,10 @@
+import Task1 from './components/Task1'
 function App() {
-	return <div className='App'>Hello</div>
+	return (
+		<div className='App'>
+			<Task1 />
+		</div>
+	)
 }
 
 export default App
